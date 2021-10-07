475244f256672b7a6c956e14a3ed2b19dc9abdd3
LosSantosSavior <nickrussell001@gmail.com>
2021-10-07 11:28:40 -0400

    3rd Commit, 10/7/21

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        WeekDay deliveryDay = WeekDay.Friday;
-        var RNG = new Random();
-        var choice = RNG.nextInt(WeekDay.values().length);
-        deliveryDay = WeekDay.values()[choice];
-        System.out.println("Your delivery day is: " + deliveryDay);
+        var allEmployees = new ArrayList<Employee>();
+        Employee newEmployee = new CommissionedEmployee();
+        allEmployees.add(newEmployee);
+        newEmployee = new SalariedEmployee(55000);
+        allEmployees.add(newEmployee);
+        //add more as needed here
+
+        for(var emp : allEmployees) {
+            System.out.println("Paying a " +emp.getClass().getName()+ " $"+emp.calculateBiWeeklyPay());
+        }
+
+        //WeekDay deliveryDay = WeekDay.Friday;
+        //var RNG = new Random();
+        //var choice = RNG.nextInt(WeekDay.values().length);
+        //deliveryDay = WeekDay.values()[choice];
+        //System.out.println("Your delivery day is: " + deliveryDay);
     }
 }
